24c59c9fcaeb7d84d053e1cf403388d02c989ce3
hadoo <hadoo8688@gmail.com>
2019-01-10 18:25:52 +0900

    Make todoPage

--- study1/src/router/index.js
+++ study1/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import TodoPage from '@/components/TodoPage'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/todos',
+      name: 'TodoPage',
+      component: TodoPage
     }
   ]
 })
